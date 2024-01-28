ad61a47724b504a6c9c7abf9fe8fcb2f386aa81a
Ricardo Rossi <ricardoalexrossi@gmail.com>
2024-01-28 12:29:59 -0300

    RDS commit

---
+++ rds.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-west-1"
+  alias  = "west1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+}
